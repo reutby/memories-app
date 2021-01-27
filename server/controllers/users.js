e8eddeba6cd69c2f68505240561312f1f166db52
reutby <reutby321@gmail.com>
2021-01-27 20:02:25 +0200

    update

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -33,9 +33,10 @@ export const signup = async (req, res) => {
         
         const hashedPassword = await bcrypt.hash(password, 12);
         const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+        console.log(process.env.SECRET_TOKEN);
         const token = jwt.sign({ email: result.email, id: result._id }, process.env.SECRET_TOKEN, { expiresIn: "1h" });
         res.status(200).json({result,token});
-
+        
     } catch (error) {
         res.status(500).json({message:'Something went wrong. '});
 
