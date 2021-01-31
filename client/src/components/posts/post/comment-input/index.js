57e255d7a0675f074b2d206992b5928de1ec578c
reutby <reutby321@gmail.com>
2021-01-31 18:30:49 +0200

    adding emoji picker to the comment input form

--- client/src/components/posts/post/comment-input/index.js
+++ client/src/components/posts/post/comment-input/index.js
@@ -1,45 +1,77 @@
-import React,{useState} from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react'
 import { TextareaAutosize, Button } from "@material-ui/core";
+import Picker from 'emoji-picker-react';
 import useStyles from "./styles/comment-input";
-import {createComment} from "../../../../store/actions/comments";
-import {useDispatch} from "react-redux"
-const CommentInput = ({postId}) => {
+import SentimentSatisfiedOutlinedIcon from '@material-ui/icons/SentimentSatisfiedOutlined';
+import { createComment } from "../../../../store/actions/comments";
+import { useDispatch } from "react-redux"
+const CommentInput = ({ postId }) => {
     const classes = useStyles();
     const [message, setMessage] = useState('');
     const dispatch = useDispatch();
     const user = JSON.parse(localStorage.getItem('profile'));
-    
-    const handleSubmit = (e)=>{
+
+    const [toggleEmojiButton, setToggleEmojiButton] = useState(false);
+    const [chosenEmoji, setChosenEmoji] = useState(null);
+
+    const onEmojiClick = (event, emojiObject) => {
+        setChosenEmoji(emojiObject);
+        setToggleEmojiButton(false);
+    };
+
+    useEffect(() => {
+        if(chosenEmoji){
+            const newMessage =  message + chosenEmoji?.emoji;
+            setMessage(newMessage);
+        }
+    }, [chosenEmoji]);
+
+    const handleSubmit = (e) => {
         e.preventDefault();
-        if(user?.result){
-            const comment ={
-              message:message,
-              name:user.result.name,
-              isGoogleLogin:(user.result?.imageUrl)?true : false,
-              avatarSrc:user.result?.imageUrl ||user.result.name
+        if (user?.result) {
+            const comment = {
+                message: message,
+                name: user.result.name,
+                isGoogleLogin: (user.result?.imageUrl) ? true : false,
+                avatarSrc: user.result?.imageUrl || user.result.name
             }
-            dispatch(createComment(postId,comment));
+            dispatch(createComment(postId, comment));
             setMessage('');
         }
     }
-    const handleChange = (e)=>{
+    const handleChange = (e) => {
         setMessage(e.target.value);
     }
     return (
-        <form className={classes.form} onSubmit={handleSubmit}>
-            <TextareaAutosize
-                placeholder="Add a comment..."
-                autoFocus
-                name= "comment"
-                value ={message} 
-                type="text"
-                onChange={handleChange}
-                className={classes.commentInput}
-            />
-            <Button type="submit" className={classes.postButton} color='primary'>
-                Post
+        <>
+            <form className={classes.form} onSubmit={handleSubmit}>
+                <div className={classes.flexBox}>
+                    <TextareaAutosize
+                        placeholder="Add a comment..."
+                        autoFocus
+                        name="comment"
+                        value={message}
+                        type="text"
+                        onChange={handleChange}
+                        className={classes.commentInput}
+                    />
+                    <Button className={classes.emojiButton} onClick={() => setToggleEmojiButton((prev) => !prev)}>
+                        <SentimentSatisfiedOutlinedIcon fontSize="default" />
+                    </Button>
+
+                </div>
+                <Button type="submit" className={classes.postButton} color='primary'>
+                    Post
             </Button>
-        </form>
+            {toggleEmojiButton &&
+                <div className={classes.emojiPicker}>
+                    <Picker disableSkinTonePicker={true} onEmojiClick={onEmojiClick} pickerStyle={{ height: '13rem',width:'19rem' }} />
+
+                </div>}
+            </form>
+
+        </>
     )
 }
 
--- client/src/components/posts/post/comment-input/styles/comment-input.js
+++ client/src/components/posts/post/comment-input/styles/comment-input.js
@@ -1,23 +1,59 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles({
-    form:{
-        display:'flex',
-        flexDirection:'rows',
-        width:'100%',
+export default makeStyles((theme) => ({
+
+    form: {
+        display: 'flex',
+        flexDirection: 'rows',
+        width: '100%',
+
     },
     commentInput: {
-        width:'100%',
-        padding: '.3rem 1rem',
+        width: '100%',
+        padding: '.3rem .5rem',
         borderBottomColor: '#000',
         borderRadius: '15px',
-        fontSize: '1rem',
+        fontSize: '1.5rem',
         backgroundColor: '#fff',
+        display: 'flex',
+    },
+    emojiButton: {
+        color: '#fff',
         display:'flex',
     },
-
     postButton: {
         fontWeight: 'bold',
-        fontSize: '1rem'
+        fontSize: '1rem',
+        display:'flex',
     },
-});
+    emojiPicker: {
+        position: 'absolute',
+        right: '6rem',
+        bottom: '4rem',
+        zIndex: '1000',
+        width: 'auto',
+        height: 'auto',
+    },
+    flexBox:{
+        display:'flex',
+        flexDirection:'row', 
+        width:'90%',   
+    },
+    [theme.breakpoints.down('sm')] :{
+        emojiPicker:{
+            width:'auto',
+            right:'.8rem',
+            bottom:'6.5rem',
+        },
+        form:{
+            flexDirection:'column',
+        },
+        commentInput: {
+            fontSize:'1rem',
+            width:'90%',
+        },
+        flexBox:{
+            width:'100%',
+        }
+    }
+}))
