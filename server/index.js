b3dcb972a7a0a4d5fd6da3558e51abda7a6fd6c1
reutby <reutby321@gmail.com>
2021-02-10 12:49:03 +0200

    updating the backend with pusher pakage in order to  enabled realtime updating of the notifications collections

--- server/index.js
+++ server/index.js
@@ -7,6 +7,16 @@ import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 import commentsRoutes from "./routes/comments.js";
 import notificationsRoutes from "./routes/notifications.js"
+import Pusher from "pusher";
+
+const pusher = new Pusher({
+    appId: "1152837",
+    key: "b9cd641863699b028250",
+    secret: "ec4cabf6faa83aecd4fc",
+    cluster: "mt1",
+    encrypted: true,
+});
+const channel = 'notifications';
 
 const app = express();
 
@@ -28,10 +38,33 @@ app.get('/', (req,res)=>{
 const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(port,() => console.log(`server running on port: ${port}`)))
-    .catch((err) => console.log(err.message));
-
 mongoose.set('useFindAndModify',false);
 
+const db = mongoose.connection;
+db.on('error',console.error.bind(console, 'Connection Error:'));
+db.once('open', ()=>{
+    app.listen(port,() => console.log(`server running on port: ${port}`));
+    const notificationsCollection = db.collection('notifications');
+    const changeStream = notificationsCollection.watch();
 
+    changeStream.on('change',(change=>{
+        switch(change.operationType){
+            case 'insert':
+                const notification = change.fullDocument;
+                pusher.trigger(channel,
+                    'inserted',{
+                    ...notification
+                });
+                return
+            case 'delete':
+                pusher.trigger(
+                    channel,
+                    'deleted',
+                    change.documentKey._id
+                );
+            default:
+                console.log(`event ${change.operationType} not handle in the switch statement`);         
+        }
+    }))
 
+})
