80756b2d408c631472cd8b70ca61460f7354674f
reutby <reutby321@gmail.com>
2021-03-03 18:05:08 +0200

    modify the error message i got from the server

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -6,20 +6,27 @@ import User from "../models/user.js";
 
 dotenv.config();
 
+const errorsMessages = {
+    userNotExist:"User doesn't exist.",
+    isPasswordCorrect:"Invalid credentials, please try again.",
+    defaultMessage:"Something went wrong.",
+    userAlreadyExist:"User already exist.",
+    passwordNotMatch:"Passwords don't match.",
+}
 export const signin = async (req, res) => {
     const { email, password } = req.body;
     try {
         const existingUser = await User.findOne({ email }).lean();
-        if (!existingUser) return res.status(404).json({ message: "user dosn't exist" });
+        if (!existingUser) return res.status(404).json({ message: errorsMessages.userNotExist });
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
 
-        if (!isPasswordCorrect) return res.status(400).json({ message: "invalid credentials" });
+        if (!isPasswordCorrect) return res.status(400).json({ message: errorsMessages.isPasswordCorrect });
 
         const token = jwt.sign({ email: existingUser.email, id: existingUser._id },  process.env.SECRET_TOKEN, { expiresIn: "1h" });
 
         res.status(200).json({ result: existingUser, token });
     } catch (error) {
-        res.status(500).json({ message: 'Something went wrong.' });
+        res.status(500).json({ message: errorsMessages.defaultMessage });
 
     }
 }
@@ -28,8 +35,8 @@ export const signup = async (req, res) => {
     const { firstName, lastName, userName, email, password, confirmedPassword,imageUrl } = req.body;
     try {
         const existingUser = await User.findOne({ email }).lean();
-        if (existingUser) return res.status(400).json({ message: "user already exist" });
-        if (password !== confirmedPassword) return res.status(400).json({ message: "Passwords don't match. " });
+        if (existingUser) return res.status(400).json({ message: errorsMessages.userAlreadyExist });
+        if (password !== confirmedPassword) return res.status(400).json({ message: errorsMessages.passwordNotMatch });
         
         const hashedPassword = await bcrypt.hash(password, 12);
         const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}`,userName,imageUrl});
@@ -37,7 +44,7 @@ export const signup = async (req, res) => {
         res.status(200).json({result,token});
         
     } catch (error) {
-        res.status(500).json({message:'Something went wrong. '});
+        res.status(500).json({message:errorsMessages.defaultMessage});
 
     }
 }
