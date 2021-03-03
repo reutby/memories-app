4cfaa2fdac4b17bf310c18c6b88087f78c62f3e9
reutby <reutby321@gmail.com>
2021-03-03 11:59:18 +0200

    render only the friends posts and my posts

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Container, Grid, Grow } from '@material-ui/core'
-import { useDispatch } from "react-redux";
-
+import { useDispatch,useSelector } from "react-redux";
 import useStyle from "./styles/home";
 import { getAllComments } from "../../store/actions/comments";
 import { getPosts } from "../../store/actions/posts";
-import { Posts, Form } from "../"
+import { fetchAllProfiles } from "../../store/actions/profiles";
+import { Posts, Form, SearchBar } from "../"
 
 const Home = () => {
 
@@ -13,23 +13,34 @@ const Home = () => {
     const [currentId, setCurrentId] = useState(null);
 
     const dispatch = useDispatch();
+    const user = useSelector(state=>state.auth.authData);
 
     useEffect(() => {
         dispatch(getPosts());
     }, [dispatch, currentId]);
 
-    
+    useEffect(() => {
+        dispatch(fetchAllProfiles());
+    }, [dispatch])
+
     useEffect(() => {
         dispatch(getAllComments());
     }, [dispatch, currentId])
+   
     return (
+
         <Grow in>
+
             <Container className={classes.topContainer}>
+
                 <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item className={classes.postsItem} xs={12} sm={8}>
-                        <Posts setCurrentId={setCurrentId} />
+                    <Grid item className={classes.postsItem} sm={12}>
+                        <SearchBar user={user} />
                     </Grid>
-                    <Grid item xs={12} sm={4}>
+                    <Grid item className={classes.postsItem} xs={12} sm={8}>
+                        <Posts userId={user?.result?.googleId || user?.result?._id} setCurrentId={setCurrentId} />
+                    </Grid>
+                   <Grid item xs={12} sm={4}>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
                     </Grid>
                 </Grid>
--- client/src/components/posts/index.js
+++ client/src/components/posts/index.js
@@ -1,35 +1,47 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import React,{useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, CircularProgress } from "@material-ui/core"
-import {useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import Post from "./post";
 import useStyles from "./styles/posts";
-import {useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
-const Posts = ({setCurrentId}) => {
+const Posts = ({ setCurrentId, userId }) => {
     const classes = useStyles();
     const location = useLocation().pathname;
-    const isHomePage = location ==='/';
-    const posts = useSelector((state) => isHomePage? state.posts : state.posts.filter((post)=>post.creator ===location.substr(1,location.length-1)));
+    const isHomePage = location === '/';
+    const profiles = useSelector((state) => state.profiles);
+    const posts = useSelector((state) => isHomePage ? state.posts : state.posts.filter((post) => post.creator === location.substr(1, location.length - 1)));
+    const [friendsPosts, setFriendsPosts] = useState(isHomePage ? null : posts);
+
     useEffect(() => {
-        
-    }, [posts])
+        if (isHomePage) {
+            const indexUser = profiles.findIndex((profile) => profile.userId === userId);
+            if (indexUser !== -1) {
+                const { followings } = profiles[indexUser];
+                setFriendsPosts(posts.filter((post) => followings.find((following) => post.creator === following || post.creator ===userId)));
+            }
+        }
+    }, [posts, profiles, userId])
     return (
-        !posts ? <CircularProgress /> :
+        !friendsPosts ? <CircularProgress /> :
             (
-                <Grid className={isHomePage?classes.posts : classes.mainContainerProfile}
+                <Grid className={isHomePage ? classes.posts : classes.mainContainerProfile}
                     container
-                    
+
                     spacing={3}>
-                    {posts.map((post) => (
+                    {friendsPosts.map((post) => (
+
                         <Grid
                             key={post._id}
                             item
                             sm={12}
-                            md={isHomePage?12:4}
-                             
+                            md={isHomePage ? 12 : 4}
                             className={classes.post}>
-                            <Post post ={post} isHome={isHomePage} setCurrentId={setCurrentId}/>
+                            <Post post={post} isHome={isHomePage} setCurrentId={setCurrentId} />
+
+
                         </Grid>
                     ))}
                 </Grid>
